8c053d661ee1e65d1dad19c1c132ec78cd3e36a2
Maria Stager <73400027+Mstager@users.noreply.github.com>
2021-08-26 10:06:44 -0600

    Update transform_CO2.R

--- Required_functions/transform_CO2.R
+++ Required_functions/transform_CO2.R
@@ -1,7 +1,7 @@
 ##this function transforms the CO2 channel according to Gwen Bachman's equation
 
 transform.CO2=function(X, VO2.channel, flow.channel){
-	##X = CO2 channel to transform
-	transformed = (X * flow.channel - (X * VO2.channel)) / (1 - X)
+	##X = CO2 channel to transform (fractional)
+	transformed = (X/100 * flow.channel - (X/100 * VO2.channel)) / (1 - X/100)
 	return(transformed)
 }
